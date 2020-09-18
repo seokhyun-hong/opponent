657ecbfec53685ca02e9da77a22863efb2d7b91d
seokhyunhong <66937402+seokhyunhong@users.noreply.github.com>
2020-09-18 15:06:41 +0900

    Delete index.js

--- 00 Hello_World/index.js
+++
@@ -1,7 +0,0 @@
-﻿var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type' : 'text/plain'});
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
