5d502c4c26e0b87afd68c27ab146da85c9a9e447
seokhyunhong <66937402+seokhyunhong@users.noreply.github.com>
2020-09-20 23:49:36 +0900

    Add files via upload

---
+++ 00 Hello_World/index.js
@@ -0,0 +1,22 @@
+﻿class Main
+{
+    constructor(req, res)
+    {
+    this.req = req;
+    this.res = res;
+    }
+
+    onRequest(req, res)
+    {
+        res.writeHead(200, {'Content-Type' : 'text/plain'});
+    	res.write('Hello World!');
+    	res.end();
+    }
+
+}
+
+var request = new Main;
+
+var http = require('http');
+
+http.createServer(request.onRequest).listen(process.env.PORT);
