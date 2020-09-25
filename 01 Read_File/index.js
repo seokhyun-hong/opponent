8be3def6bbda1a469fe91baf177c9f09b558fef8
seokhyunhong <66937402+seokhyunhong@users.noreply.github.com>
2020-09-25 09:26:35 +0900

    Add files via upload

---
+++ 01 Read_File/index.js
@@ -0,0 +1,75 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var server = http.createServer(response);
+
+    server.listen(process.env.PORT);
+
+function response(req, res)
+{
+    var _url = req.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname == '/')
+        {
+            fs.readFile(`data/${queryData.id}`+'.js', 'utf8', onDescription);
+        }
+
+    else
+        {
+            return res.writeHead(404);
+        }
+
+    function onDescription(err, description)
+    {
+        var title = queryData.id;
+
+        if (queryData.id == undefined)
+            {
+                title  = '<br>';
+                title += '<br>';
+
+                description = 'document.write("Hello_World");';
+            }
+
+            /*
+            res.writeHead(200, {'Content-Type' : 'text/plain'});
+            */
+
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write(templateHTML(title, description));
+        res.end();
+
+    }
+
+    function templateHTML(title, description)
+    {
+        return `
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    <header>
+                        <h1> OPPONENT </h1>
+                    </header>
+                    <nav>
+                        <a href="/?id=${title}">${title}</a>
+                    </nav>
+                    <section>
+                        <script type="text/javascript">
+                            ${description}
+                        </script>
+                    </section>
+                    <footer>
+                    </footer>
+                </body>
+            </html>
+        `;
+    }
+}
